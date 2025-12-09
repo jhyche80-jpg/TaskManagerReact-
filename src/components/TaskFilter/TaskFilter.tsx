913c354c9e7d640fa7dc5c68ff7a1902d3f6b3e8
Javon <jhyche80@gmail.com>
2025-12-09 08:07:34 -0500

    i made the filter for the to filter the list

--- src/components/TaskFilter/TaskFilter.tsx
+++ src/components/TaskFilter/TaskFilter.tsx
@@ -1,10 +1,41 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import type { TaskFilterProps } from '../types'
 
-export default function TaskFilter() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function TaskFilter({ onFilterChange }: TaskFilterProps) {
+    const [status, setStatus] = useState()
+    const [priority, setPriority] = useState()
+    const updateFilter = (key, value) => {
+        const filters = {
+            status, priority, [key]: value,
+        }
+        onFilterChange(filters)
+    }
+    return (
+        <div className='taskFilter'>
+            <select
+                name=""
+                id=""
+                onChange={(e) => {
+                    setStatus(e.target.value)
+                    updateFilter('status', e.target.value)
+                }}
+            >
+                <option value="">All Status</option>
+                <option value="pending">Pending</option>
+                <option value="in-progress">In progress</option>
+                <option value="completed">Completed</option>
+            </select>
+
+            <select onChange={(e) => {
+                setPriority(e.target.value)
+                updateFilters('priority', e.target.value)
+            }}>
+                <option value="">All Priority</option>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+        </div>
+    )
 }
