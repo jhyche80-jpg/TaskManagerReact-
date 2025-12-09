8c71c7915b6267671640054d92767f4eae0c133c
Javon <jhyche80@gmail.com>
2025-12-09 08:32:57 -0500

    I made the inputs

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -6,23 +6,43 @@ import TaskFilter from './TaskFilter/TaskFilter'
 import TaskList from './TaskList/TaskList'
 
 export default function Render() {
-
+ 
   const [task,setTask]= useState<Task[]>([])
-
+  const [date,setDate]= useState()
   const [filters,setFilters]= useState<{
     status?: TaskStatus
     priority?: 'low' | 'medium' | 'high'
   }>({})
-const handleFilterChange = (newfilters)=>{
+  // Handle filter change 
+const handleFilterChange = (newfilters:any)=>{
   setFilters(newfilters)
 }
+// status change 
 const handleStatusChange = (taskid:string, newStatus:TaskStatus)=>{
   setTask(prev=>prev.map(task=>task.id === taskid?{...task,status: newStatus}:task))
 
 }
-const 
+const handleDelete = (taskid:string)=>{
+  setTask(prev=> prev.filter(task=> task.id !== taskid))
+}
+//Apply the filter 
+const filteredTask = task.filter(task=>{
+  if(filters.status && task.status !== filters.status) return false
+  if (filters.priority && task.priority !== filters.priority) return false
+  return true 
+})
 
   return (
-    
+    <>
+    <input type="text" />
+    <input type="date" value={date}
+    onChange={{(e)}}
+    />
+    <TaskFilter onFilterChange={handleFilterChange}/>
+    <TaskList tasks={filteredTask}
+    onStatusChange={handleStatusChange}
+    onDelete={handleDelete}/>
+
+    </>
   )
 }
