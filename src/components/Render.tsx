1bb25bb573f9dee21d86c68b7ae98853f037111f
Javon <jhyche80@gmail.com>
2025-12-09 08:24:26 -0500

    i made the filterr change and the ststus change function in the render tab

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import type { Task, TaskStatus } from './types'
 import TaskItem from './taskItem/TaskItem'
+import { useState } from 'react'
+import TaskFilter from './TaskFilter/TaskFilter'
+import TaskList from './TaskList/TaskList'
 
 export default function Render() {
 
+  const [task,setTask]= useState<Task[]>([])
 
-    
+  const [filters,setFilters]= useState<{
+    status?: TaskStatus
+    priority?: 'low' | 'medium' | 'high'
+  }>({})
+const handleFilterChange = (newfilters)=>{
+  setFilters(newfilters)
+}
+const handleStatusChange = (taskid:string, newStatus:TaskStatus)=>{
+  setTask(prev=>prev.map(task=>task.id === taskid?{...task,status: newStatus}:task))
+
+}
+const 
+
   return (
     
   )
